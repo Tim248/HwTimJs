9a60274cdcfee3ec40a8fe4038fb6fce5e8f9e3d
Tim <tnematjonov@mail.ru>
2021-07-12 21:45:11 +0500

    End

---
+++ main.js
@@ -0,0 +1,22 @@
+var Name = prompt('Напишите свое имя');
+var Age = +prompt('Напишите свой возраст');
+var UserAddition = +prompt('Решите несколько задач ' + Name + ' :  7 + 6 = ?');
+var UserSubtraction = +prompt('12 - 7 = ?');
+var UserMultiplication = +prompt('9 * 8 = ?');
+var UserDivision = +prompt('48 / 2 = ?');
+var USerReDivision = +prompt('7 % 3 = ?');
+
+prompt('!!!! Зайдите в консоль !!!!')
+
+var Addition = 7 + 6;
+var Subtraction = 12 - 7;
+var Multiplication = 9 * 8;
+var Division = 48 / 2;
+var ReDivisionn = 7 % 3;
+
+
+console.log('7 + 6 = ' + Addition + ' (Ваш ответ: ' + UserAddition + ')');
+console.log('12 - 7 = ' + Subtraction + ' (Ваш ответ: ' + UserSubtraction + ')');
+console.log('9 * 8 = ' + Multiplication + ' (Ваш ответ: ' +UserMultiplication + ')');
+console.log('48 / 2 = ' + Division + ' (Ваш ответ: ' + UserDivision + ')');
+console.log('7 % 3 = ' + ReDivisionn + ' (Ваш ответ: ' + USerReDivision + ')');
